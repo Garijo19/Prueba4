3ae43dfc198c2035480a276062a6b1f442d899b0
Garijo19 <franciscojosegarijo@gmail.com>
2024-01-10 09:40:39 +0100

    Commit 1: Practica prueba

---
+++ Prueba4/src/main/java/com/proyecto/prueba4/Prueba4.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.proyecto.prueba4;
+
+/**
+ *
+ * @author daw1_
+ * 
+ */
+public class Prueba4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Adios");
+    }
+}
